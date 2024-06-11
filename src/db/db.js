40727ce82bc7fdd55f8b9aaf984bc89c113cc138
Braikan123 <braikanpisa2604@gmail.com>
2024-06-11 02:52:58 -0500

    fixeando

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,5 @@
 import { createPool } from 'mysql2/promise';
-import {
-    DB_HOST,
-    DB_NAME,
-    DB_PASSWORD,
-    DB_USER,
-    DB_PORT
-} from './settings.js'
+
   
 export const pool = createPool({
     host: DB_HOST,
